f5ee36b989158c3518bcac90152ec31dbfe4d23b
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-08-01 19:34:18 +0530

    Add files via upload

---
+++ Selectionsort.java
@@ -0,0 +1,34 @@
+﻿package com.company;
+import java.util.Scanner;
+class Sorting
+{
+    public static void s()
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i,j,temp;
+        int []a = new int[n];
+        for(i=0;i<n;i++)
+            a[i]=in.nextInt();
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(a[i]<a[j])
+                {
+                    temp= a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        System.out.println(a[i]);
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+	Sorting s = new Sorting();
+    s.s();
+    }
+}
